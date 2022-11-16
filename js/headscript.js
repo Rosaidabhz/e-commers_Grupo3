a24fd2307df31243ca69ed76c4125933a095527e
ManeDM <mane657@hotmail.com.com>
2022-11-16 18:01:16 -0500

    despliegue de menu

--- js/headscript.js
+++ js/headscript.js
@@ -1,16 +1,3 @@
-addEventListener("DOMContentLoaded", () => {
-    const bars_menu = document.querySelector(".bars_menu")
-    if (bars_menu) {
-        bars_menu.addEventListener("click", () => {
-            const head_link = document.querySelector(".head_link")
-            head_link.classList.toggle("mostrar")
-        })
-    }
-})
-
-
-
-
 document.querySelector(".bars_menu").addEventListener("click", animateBars);
 
 var line1_bars = document.querySelector(".line1_bars_menu");
@@ -22,3 +9,5 @@ function animateBars(){
     line2_bars.classList.toggle("activeline2_bars_menu");
     line3_bars.classList.toggle("activeline3_bars_menu");
 }
+
+
