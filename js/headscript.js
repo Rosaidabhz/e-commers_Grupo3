4abb5a818caaad221c4a40822c143b5b5fade9ac
ManeDM <mane657@hotmail.com.com>
2022-11-17 11:22:30 -0500

    penultimo paso

--- js/headscript.js
+++ js/headscript.js
@@ -3,11 +3,14 @@ document.querySelector(".bars_menu").addEventListener("click", animateBars);
 var line1_bars = document.querySelector(".line1_bars_menu");
 var line2_bars = document.querySelector(".line2_bars_menu");
 var line3_bars = document.querySelector(".line3_bars_menu");
+var list = document.querySelector(".menu_items")
 
 function animateBars(){
     line1_bars.classList.toggle("activeline1_bars_menu");
     line2_bars.classList.toggle("activeline2_bars_menu");
     line3_bars.classList.toggle("activeline3_bars_menu");
+
+    list.classList.toggle("activemenu_items")
 }
 
 
